304e422876353f52857a9c9f22839b0dd7a4f6f9
hktari <bkamnik1995@gmail.com>
2022-11-15 13:51:23 +0100

    add testing sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+import { MDBBtn, MDBContainer, MDBInputGroup } from 'mdb-react-ui-kit';
+import { MDBInput } from 'mdb-react-ui-kit';
 
 function App() {
+
+  function performLogin(){
+    console.log('logging in...')
+  }
+  
   return (
     <MDBContainer fluid>
-      <div
-        className='d-flex justify-content-center align-items-center'
-        style={{ height: '100vh' }}
-      >
-        <div className='text-center'>
-          <img
-            className='mb-4'
-            src='https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'
-            style={{ width: 250, height: 90 }}
-          />
-          <h5 className='mb-3'>
-            Thank you for using our product. We're glad you're with us.
-          </h5>
-          <p className='mb-3'>MDB Team</p>
-          <MDBBtn
-            tag='a'
-            href='https://mdbootstrap.com/docs/standard/getting-started/'
-            target='_blank'
-            role='button'
-          >
-            Start MDB tutorial
-          </MDBBtn>
-        </div>
-      </div>
+      <MDBInputGroup className='mb-3' >
+        <input className='form-control' type='email' placeholder="Email" />
+      </MDBInputGroup>
+
+      <MDBInputGroup className='mb-3' >
+        <input className='form-control' type='password' placeholder="Password" />
+      </MDBInputGroup>
+
+      <MDBBtn onClick={performLogin}>Login</MDBBtn>
     </MDBContainer>
   );
 }
