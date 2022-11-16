34a7f0f9853c4ade16d236eaeb3c933451d8308e
hktari <bkamnik1995@gmail.com>
2022-11-16 11:09:13 +0100

    add implementation of logs service

--- src/interface/common.interface.ts
+++ src/interface/common.interface.ts
@@ -15,3 +15,14 @@ export interface Credentials {
     password_cipher: string,
     iv_cipher: string
 }
+
+
+export interface LogEntry {
+    id:          string;
+    status:      string;
+    timestamp:   Date;
+    error:       null;
+    message:     string;
+    action:      string;
+    config_type: string;
+}
---
+++ src/services/account/logs.service.ts
@@ -0,0 +1,17 @@
+import { LogEntry } from "../../interface/common.interface"
+import http from "../http"
+
+function all(accountId: string): Promise<LogEntry[]> {
+    return http.get(`/account/${accountId}/log-entry`)
+}
+
+function deleteAll(accounId: string): Promise<{ deletedCount: number }> {
+    return http.delete(`account/${accounId}/log-entry`)
+}
+
+const logsService = {
+    all,
+    deleteAll
+}
+
+export default logsService
