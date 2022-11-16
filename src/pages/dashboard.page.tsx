92a60b0ecf012d34d7d51e69acf4bce639ca06c8
hktari <bkamnik1995@gmail.com>
2022-11-16 14:06:30 +0100

    add refresh log table interval

--- src/pages/dashboard.page.tsx
+++ src/pages/dashboard.page.tsx
@@ -12,18 +12,33 @@ const DashboardPage = (props: DashboardPageProps) => {
   const { user, updateUser } = useAuth()
   const [logs, setLogs] = useState<LogEntry[]>([])
   const [updatingAutomationEnabled, setUpdatingAutomationEnabled] = useState(false)
+  const [updatingLogTable, setUpdatingLogTable] = useState(false)
+  const refreshLogsIntervalMs = 5000
 
+  // refresh log table on interval
   useEffect(() => {
-    async function fetchLogs() {
-      try {
-        setLogs(await logsService.all("1"))
-      } catch (error) {
-        console.error('failed to fetch logs', error)
-      }
+    const interval = setInterval(fetchLogs, refreshLogsIntervalMs)
+    return () => {
+      clearInterval(interval)
     }
+  }, [user])
 
+  // refresh log table on init
+  useEffect(() => {
     fetchLogs()
   }, [user])
+
+  async function fetchLogs() {
+    try {
+      console.log('fetching logs...')
+      setUpdatingLogTable(true)
+      setLogs(await logsService.all(user?.id!))
+    } catch (error) {
+      console.error('failed to fetch logs', error)
+    } finally {
+      setUpdatingLogTable(false)
+    }
+  }
 
   async function setAutomationEnabled(enabled: boolean) {
     try {
@@ -53,7 +68,10 @@ const DashboardPage = (props: DashboardPageProps) => {
       </section>
 
       <section data-section="logs">
-        <h2 className="mb-3">Dnevnik</h2>
+        <div className="d-flex align-items-center mb-3">
+          <h2 className="mb-0 flex-grow-1">Dnevnik</h2>
+          <MDBSpinner className={!updatingLogTable ? 'd-none' : ''} size='sm' role='status' tag='span' />
+        </div>
         <LogTable data={logs} />
       </section>
 
