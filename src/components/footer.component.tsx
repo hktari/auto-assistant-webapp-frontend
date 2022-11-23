96ae6b355de19cfc1ca03b00e47151c3737716b8
hktari <bkamnik1995@gmail.com>
2022-11-23 14:14:13 +0100

    update build script

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -8,7 +8,6 @@ const Footer = (props: FooterProps) => {
     return (
         <MDBFooter className='text-muted h-100'>
             <div className="d-flex align-items-center justify-content-center h-100">
-                <a className='text-reset' href="https://www.bostjankamnik.com">bkamnik 2022</a>
             </div>
         </MDBFooter>
     )
