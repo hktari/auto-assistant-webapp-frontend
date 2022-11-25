276d6513036444c131174c12112eb18b8a074b56
hktari <bkamnik1995@gmail.com>
2022-11-25 12:18:00 +0100

    add removeAlert to provider

--- src/providers/alert.provider.tsx
+++ src/providers/alert.provider.tsx
@@ -7,17 +7,26 @@ export enum AlertType {
     warning
 }
 
-export interface Alert {
+export class Alert {
+    private static idCntr: number = 0
+    id: number
     message: string
     type: AlertType
+
+    constructor(message: string, type: AlertType) {
+        this.id = Alert.idCntr++
+        this.message = message
+        this.type = type
+    }
 }
 
 interface AlertContext {
     alerts: Alert[]
     addAlert: (alert: Alert) => void
+    removeAlert: (alert: Alert) => void
 }
 
-const AlertContext = createContext<AlertContext>({ alerts: [], addAlert: null! })
+const AlertContext = createContext<AlertContext>({ alerts: [], addAlert: null!, removeAlert: null! })
 
 
 const AlertProvider = ({ children }: { children: React.ReactNode }) => {
@@ -27,7 +36,18 @@ const AlertProvider = ({ children }: { children: React.ReactNode }) => {
         setAlerts([...alerts, alert])
     }
 
-    const value = { alerts, addAlert }
+    function removeAlert(alert: Alert) {
+        const toRemoveIdx = alerts.findIndex(a => a.id === alert.id)
+        if (toRemoveIdx === -1) {
+            throw new Error('failed to remove alert')
+        }
+
+        const tmp = [...alerts]
+        tmp.splice(toRemoveIdx, 1)
+        setAlerts(tmp)
+    }
+
+    const value = { alerts, addAlert, removeAlert }
     return <AlertContext.Provider value={value}>{children}</AlertContext.Provider>
 }
 
