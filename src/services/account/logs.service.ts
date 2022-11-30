e67b357f094b931f04378a9837c9a3ddc77b6dd8
hktari <bkamnik1995@gmail.com>
2022-11-30 13:45:58 +0100

    wrap timestamp in new Date()
    this results in the api returning ISO formatted timestamps

--- src/services/account/logs.service.ts
+++ src/services/account/logs.service.ts
@@ -2,39 +2,6 @@ import { LogEntry } from "../../interface/common.interface"
 import { ParseDatesReviver } from "../../util/json.util"
 import http from "../http"
 
-function mockLogs() {
-    const mockLogsData = `[
-        {
-            "id": "6",
-            "status": "successful",
-            "timestamp": "2022-11-15 07:10:00",
-            "error": null,
-            "message": "Finished successfully !",
-            "action": "start_btn",
-            "config_type": "CONFIG_TYPE_WEEKLY"
-        },
-        {
-            "id": "7",
-            "status": "successful",
-            "timestamp": "2022-11-15 07:15:00",
-            "error": null,
-            "message": "Finished successfully !",
-            "action": "stop_btn",
-            "config_type": "CONFIG_TYPE_WEEKLY"
-        },
-        {
-            "id": "7",
-            "status": "failed",
-            "timestamp": "2022-11-15 07:15:00",
-            "error": "error",
-            "message": null,
-            "action": "stop_btn",
-            "config_type": "CONFIG_TYPE_WEEKLY"
-        }
-    ]`
-    return JSON.parse(mockLogsData, ParseDatesReviver)
-}
-
 function all(accountId: string): Promise<LogEntry[]> {
     return http.get(`/account/${accountId}/log-entry`)
 }
